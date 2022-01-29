9bede4b6b2f630be1f9bc0abe46bd2e0f0e88efc
H-hidetaka <sansanhidetaka@gmail.com>
2022-01-29 13:06:37 +0900

    [Modify]プロフィール画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # application_controller.rbに「:authenticate_user!」を設定しているのでどのページでもログインしていないとログインページに飛ばされる。もし、ログインが必要なページと必要じゃないページを分けたいのなら、ログインが必要なページに都度[before_action :authenticate_user!]を書くか、２つ目のアプリケーションコントローラーを作ってそこにapplication_controllerを継承、authenticate~を記述し、必要なコントローラーに継承する形でもいいかもしれない。
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    "(ログイン後に遷移させたページ)"
+    user_session_path
   end
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    root "users/sessions#new"
-  end
+  # resources :users, only: [:show]
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  get 'users/show'
+  # devise_scope :user do
+  #   root "users/sessions#new"
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
